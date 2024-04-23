bdf3d4be80936a1e893ac52c2f4d82e19809223d
kagaya <yuhei.kagaya@gmail.com>
2024-04-23 14:07:16 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -60,6 +60,13 @@ if type -q nodenv
   nodenv init - | source
 end
 
+# direnv
+if type -q direnv
+  direnv hook fish | source
+  set -g direnv_fish_mode eval_on_arrow    # trigger direnv at prompt, and on every arrow-based directory change (default)
+  # set -g direnv_fish_mode eval_after_arrow # trigger direnv at prompt, and only after arrow-based directory changes before executing command
+  # set -g direnv_fish_mode disable_arrow    # trigger direnv at prompt only, this is similar functionality to the original behavior
+end
 
 # dircolors
 if type -q gdircolors
